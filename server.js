0bbef91865bda96df0d7d3859eab232e38f05186
Puxuan He <puxuanhe@gmail.com>
2016-11-07 22:24:19 -0600

    add validation

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 var express = require('express')
 var config = require('./src/config.js')
+var crypto = require('crypto')
+
 var app = express()
 var PORT = process.env.PORT || 8080
 
+var token='wechat-ck-client19881117';
+
 app.get('/interface', function (req, res) {
+  var signature = req.query.signature
+  var timestamp = req.query.timestamp
   var echostr = req.query.echostr
-  res.send(echostr)
+  var nonce = req.query.nonce
+
+  var oriArray = [nonce, timestamp, token]
+  oriArray.sort()
+  var scyptoString = crypto.createHash('sha1').update(oriArray.join('')).digest('hex')
+  if (signature === scyptoString) {
+    res.send(echostr)
+  } else {
+    res.send('invalid')
+  }
 })
 
 app.get('/config', function (req, res) {
