6a655987105bfedac86cf992dc5b931f610aacae
Puxuan He <puxuanhe@gmail.com>
2016-11-07 22:39:04 -0600

    add cache token

--- src/config.js
+++ src/config.js
@@ -7,8 +7,25 @@ var appId = 'wxa82cbb188ae6206a'
 var appSecret = 'ff1674f69ef14bf1535d79fe45465fce'
 var url = 'http://wechat-ck-client.herokuapp.com/'
 
+var cacheToken = null
+var tokenTimeStamp = null
+
+function checkCachedToken () {
+  return cacheToken && (new Date().getTime() / 1000 - tokenTimeStamp < 7200)
+}
+
+function setCacheToken (token) {
+  cacheToken = token
+  tokenTimeStamp = new Date().getTime() / 1000
+}
 
 function getToken(cb) {
+  if (checkCachedToken()) {
+    console.info('using cache token')
+    cb(null, cacheToken)
+    return
+  }
+  console.info('requesting new token')
   var tokenUrl = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appId=' + appId + '&secret=' + appSecret
   request.get(tokenUrl, function(error, response, body) {
     if (error) {
@@ -16,6 +33,7 @@ function getToken(cb) {
     } else {
       try {
         var token = JSON.parse(body).access_token
+        setCacheToken(token)
         cb(null, token)
       } catch (e) {
         cb('getToken error', e)
